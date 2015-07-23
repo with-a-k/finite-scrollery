f86e0ac8b6374a12e17bf53ed304e487e7738b86
Erik Butcher <with_a_k@mac.com>
2015-07-23 14:45:36 -0600

    Change on welcome page path

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def show
-    if current_user
+    if current_user && current_user.text_posts_only
       @posts = current_user.text_posts_only
     end
   end
